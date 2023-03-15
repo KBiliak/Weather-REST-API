8e710ae40e8fd30c8f9de1011342d3e90638f53d
Kate Biliak <kate.bilyak@gmail.com>
2023-03-15 11:44:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask("Website")
+
+
+@app.route('/')
+def home():
+    return render_template('hom e.html')
+
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = 23
+    return {'station': station,
+            'date': date,
+            'temperature': temperature}
+
+
+app.run(debug=True)
